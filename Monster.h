63998699bf13802f1951b6d92ce38460b2ffacac
dxgrxx0 <163234450+dxgrxx0@users.noreply.github.com>
2024-11-11 21:39:41 +0900

    몬스터 종류 추가, 방어력 구현, 몬스터의 공격 구현

--- Monster.h
+++ Monster.h
@@ -3,14 +3,22 @@
 #define MONSTER_H
 
 #include <SFML/Graphics.hpp>
-
+class Character;
+enum class MonsterType {
+    Basic,
+    Speed,
+    Attack,
+    Defense,
+    Mid_Boss,
+    Main_Boss
+};
 class Monster {
 public:
     // ������
-    Monster(float x, float y, float speed);
+    Monster(float x, float y, float speed,MonsterType type);
 
     // ���� ������Ʈ �Լ� (��ǥ ��ġ�� �̵�)
-    void update(sf::Vector2f targetPosition, float deltaTime);
+    void update(const sf::Vector2f& CharacterPos,const sf::Vector2f& MainTowerPos, float deltaTime,Character& character);
 
     // ���͸� �����쿡 �׸��� �Լ�
     void draw(sf::RenderTarget& target)const;
@@ -32,7 +40,11 @@ private:
     bool isTakingDamage; // 피해를 표시해야 하는지 여부
     float damageDisplayDuration; // 피해 표시 지속 시간
     float damageDisplayTime; // 피해 표시 경과 시간
+    float attackPower; //공격력
+    float defense;  //방어력
 
+    sf::Clock attackTimer; // 공격 타이머 추가
+    float attackCooldown = 1.0f; // 1초 주기
 };
 
 #endif // MONSTER_H
