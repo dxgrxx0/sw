fd7522ae2824bac985efdd6e226e57ed4e631e50
dxgrxx0 <163234450+dxgrxx0@users.noreply.github.com>
2024-12-05 16:14:27 +0900

    투사체 회전 속도 조절

--- BombTower.h
+++ BombTower.h
@@ -82,7 +82,7 @@ public:
         for (auto it = projectiles.begin(); it != projectiles.end();) {
             it->update(deltaTime);
             bool projectileDestroyed = false;
-			it->sprite.rotate(10.0f); // 투사체 회전
+			it->sprite.rotate(deltaTime*360.0f); // 투사체 회전
             if (it->isOutofBound()) {
                 it = projectiles.erase(it);
                 continue;
